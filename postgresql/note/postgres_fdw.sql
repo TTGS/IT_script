46c34e2b964a4e05ea390d200d02700cd5baf04c
TT <foggyglass@163.com>
2020-03-25 19:49:03 +0800

    Update postgres_fdw.sql

--- postgresql/note/postgres_fdw.sql
+++ postgresql/note/postgres_fdw.sql
@@ -1,6 +1,3 @@
-创建后，需要给用户授权查询forgine表权限，使用的是  grant select on ALL tables in schema public TO cross_view ;命令 
- 
- 
 --如果本库需要，那么本库就需要安装。
 CREATE EXTENSION postgres_fdw;
 
@@ -17,6 +14,7 @@ CREATE USER MAPPING FOR local_user
         OPTIONS (user 'foreign_user', password 'password');
 
  -- 创建的外部表，是没有单独授权的内容。
+ --创建后，需要给用户授权查询forgine表权限，使用的是  grant select on ALL tables in schema public TO cross_view ;
 CREATE FOREIGN TABLE foreign_table (
         id integer NOT NULL,
         data text
@@ -25,3 +23,6 @@ CREATE FOREIGN TABLE foreign_table (
         OPTIONS (schema_name 'some_schema', table_name 'some_table');
 
 
+这里注意一个事情，如果你已经配置好了，那么不需要以下两个授权。
+grant USAGE on FOREIGN server hp_server TO cross_view ; 
+grant USAGE on FOREIGN data wrapper postgres_fdw to cross_view ;
